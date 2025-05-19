38670a72dd0d955d5c1b269f56b18f35982fe7a9
myriosMin <minmyrios@gmail.com>
2025-05-19 21:48:29 +0800

    Base template and individual tasks

--- src/the_team/utils/etl.py
+++ src/the_team/utils/etl.py
@@ -1,10 +1,13 @@
 """Write ETL functions here like data loading, cleaning, and transformation."""
 
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
 import pandas as pd
+from pathlib import Path
 
-def load_csv(file_path):
+def load_csv(file_path: Path) -> pd.DataFrame:
     """
-    Load a CSV file into a pandas DataFrame.
+    Load a CSV file into a pandas DataFrame. Downloads the file if it does not exist locally.
     
     Args:
         file_path (str): Path to the CSV file.
@@ -12,6 +15,18 @@ def load_csv(file_path):
     Returns:
         pd.DataFrame: Loaded DataFrame.
     """
+    # Download the file if it does not exist (fixed to Olist dataset from Kaggle for demonstration)
     if not file_path.exists():
-        raise FileNotFoundError(f"{file_path} does not exist. Please check the path.")
+        print(f"File {file_path.name} not found. Downloading from Kaggle...")
+        
+        df = kagglehub.load_dataset(
+        KaggleDatasetAdapter.PANDAS,
+        "olistbr/brazilian-ecommerce",
+        file_path.name,
+        )
+
+        df.to_csv(file_path, index=False)
+        print(f"File {file_path.name} downloaded successfully and loaded into {file_path.parent}.")
+        
+    # Returns the pandas DataFrame    
     return pd.read_csv(file_path) 
